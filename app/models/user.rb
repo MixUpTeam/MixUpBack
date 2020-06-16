954c2f43d513ae604a9041d530448607d6c43ed3
Yousri <laifayousri@gmail.com>
2020-06-16 18:56:10 +0200

    [edit] User model add dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 					:recoverable, :rememberable, :validatable
 
 	has_one :user_profile, dependent: :destroy
-	has_many :playlists, foreign_key: :owner_id
+	has_many :playlists, foreign_key: :owner_id, dependent: :destroy
 
 	def create_profile    
 		UserProfile.create(user: self)    
