8f656f4adfbb0a91bdca0b76d8514f5ba6fe5248
floriansr <sueurfloriansf@gmail.com>
2020-06-17 12:36:45 +0200

    [run] migration passed

--- db/migrate/20200617101548_create_track_playlists.rb
+++ db/migrate/20200617101548_create_track_playlists.rb
@@ -1,12 +1,11 @@
 class CreateTrackPlaylists < ActiveRecord::Migration[6.0]
   def change
     create_table :track_playlists do |t|
-      t.references :added_by, null: false, foreign_key: true
+      t.references :added_by, index: true, null: false, foreign_key: { to_table: :users }
       t.string :track_spotify_id
       t.references :playlist, null: false, foreign_key: true
-      t.boolean :is_played
-      t.boolean :is_playing
-
+      t.boolean :is_played, default: false
+      t.boolean :is_playing, default: false
       t.timestamps
     end
     add_index :track_playlists, :track_spotify_id
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_123023) do
+ActiveRecord::Schema.define(version: 2020_06_17_101548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "name"
+    t.bigint "owner_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_playlists_on_name"
+    t.index ["owner_id"], name: "index_playlists_on_owner_id"
+  end
+
+  create_table "track_playlists", force: :cascade do |t|
+    t.bigint "added_by_id", null: false
+    t.string "track_spotify_id"
+    t.bigint "playlist_id", null: false
+    t.boolean "is_played", default: false
+    t.boolean "is_playing", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["added_by_id"], name: "index_track_playlists_on_added_by_id"
+    t.index ["is_played"], name: "index_track_playlists_on_is_played"
+    t.index ["is_playing"], name: "index_track_playlists_on_is_playing"
+    t.index ["playlist_id"], name: "index_track_playlists_on_playlist_id"
+    t.index ["track_spotify_id"], name: "index_track_playlists_on_track_spotify_id"
+  end
 
   create_table "user_profiles", force: :cascade do |t|
     t.string "username", default: "", null: false
@@ -36,5 +60,8 @@ ActiveRecord::Schema.define(version: 2020_06_16_123023) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "playlists", "users", column: "owner_id"
+  add_foreign_key "track_playlists", "playlists"
+  add_foreign_key "track_playlists", "users", column: "added_by_id"
   add_foreign_key "user_profiles", "users"
 end
