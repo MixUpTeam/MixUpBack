23f1c39dd25ef1cc07f2f59791c8cf06186cb859
floriansr <sueurfloriansf@gmail.com>
2020-06-17 13:38:33 +0200

    [edit] seeds.rb trackplaylists added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
 require 'faker'
 
+TRACK_SPOTIFY_IDS = ["55p8TQ1ggGYOO1gLQrC52D",
+"1sFstGV1Z3Aw5TDFCiT7vK",
+"4rHIfYRQw6ittRQXAlurk0",
+"1scwknKtBQpreYy6MSoJqJ",
+"2PR4jVXzyjCky2PkONvipq",
+"0cppqb4vQycORHNMxr8DGd",
+"2uVNn2pFpDlx1XrFcEfary"]
+
 puts "clearing records"
 
 User.destroy_all
+
+#################### User ####################
 
 puts "generating users and profiles"
 
@@ -15,13 +25,23 @@ end
 
 puts "#{User.count} users generated"
 
-# Playlist
+#################### Playlist ####################
 
 puts "generating playlists"
 
-
-10.times do
+30.times do
 	Playlist.create(name: Faker::Music.genre, owner: User.all.sample)
 end
 
 puts "#{Playlist.count} playlists generated"
+
+
+#################### TrackPlaylist ####################
+
+puts "generating trackplaylists"
+
+1000.times do
+	TrackPlaylist.create(added_by: User.all.sample, track_spotify_id: TRACK_SPOTIFY_IDS.sample, playlist: Playlist.all.sample)
+end
+
+puts "#{TrackPlaylist.count} trackplaylists generated"
