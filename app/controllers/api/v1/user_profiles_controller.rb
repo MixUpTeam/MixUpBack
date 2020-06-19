07f3d8349f4bc58d387bd06c93ffd267e9bbef3f
Yousri LAIFA <32313527+Yous91@users.noreply.github.com>
2020-06-19 11:20:31 +0200

    21 set up versionnized api path (#27)
    * [edit] routes.rb add namespace
    
    * [change] controllers location to versionnized namespace api/v1
    
    * [add] intermediary api_controller, from which other controllers inherit

---
+++ app/controllers/api/v1/user_profiles_controller.rb
@@ -0,0 +1,49 @@
+module Api 
+	module V1
+		class UserProfilesController < Api::ApiController
+			before_action :set_user_profile, only: %i[show update destroy]
+
+			def index
+				@user_profiles = UserProfile.all
+
+				render json: @user_profiles
+			end
+
+			def show
+				render json: @user_profile
+			end
+
+			def create
+				@user_profile = UserProfile.new(user_profile_params)
+
+				if @user_profile.save
+					render json: @user_profile, status: :created
+				else
+					render json: @user_profile.errors, status: :unprocessable_entity
+				end
+			end
+
+			def update
+				if @user_profile.update(user_profile_params)
+					render json: @user_profile
+				else
+					render json: @user_profile.errors, status: :unprocessable_entity
+				end
+			end
+
+			def destroy
+				@user_profile.destroy
+			end
+
+			private
+
+			def set_user_profile
+				@user_profile = UserProfile.find(params[:id])
+			end
+
+			def user_profile_params
+				params.require(:user_profile).permit(:username, :user_id)
+			end
+		end
+	end
+end
