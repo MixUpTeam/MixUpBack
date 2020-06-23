9648dfdfbd5bd7501632bf4a386da3f139b5b034
julienemo <juliette.nada@gmail.com>
2020-06-23 12:09:06 +0200

    [add] tests on sign up requests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "User auth", type: :request do
       @current_number_of_users = User.all.count
     end
 
-    it "creates user, returns id and token" do
+    it "creates user, returns id" do
       post user_registration_path, params: {
         user: {
           email: "random_email@yopmail.com",
@@ -36,6 +36,40 @@ RSpec.describe "User auth", type: :request do
       expect(response).not_to have_http_status(201)
       expect(User.all.count).to eq(@current_number_of_users + 1)
       expect(JSON.parse(response.body)["errors"]).to be_present
+    end
+    
+  end
+
+  describe "post /users/sign_in.json" do
+
+    before(:all) do
+      @user = User.create(
+        email: "random_email@yopmail.com",
+        password: "111111"
+      )
+      @current_number_of_users = User.all.count
+    end
+
+    it "doesn't creates new user, returns id" do
+      post new_user_session_path, params: {
+        user: {
+          email: @user.email,
+          password: @user.password
+        }
+      }
+      expect(response).to have_http_status(201)
+      expect(User.all.count).to eq(@current_number_of_users)
+      expect(JSON.parse(response.body)["id"]).to eq(@user.id)
+    end
+
+    it "returns errors in case of identification errors" do
+      post new_user_session_path, params: {
+        user: {
+          email: @user.email,
+          password: "#{@user.password}11"
+        }
+      }
+      expect(JSON.parse(response.body)["error"]).to be_present
     end
     
   end
