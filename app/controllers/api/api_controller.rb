736d74bf9f5e61d006030578d74ff91b0c5f7060
julienemo <juliette.nada@gmail.com>
2020-06-23 21:10:28 +0200

    [change] reponse method from application controller to api controller

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -18,10 +18,36 @@ module Api
         @user = current_user
       else
         render json: {
-          status: "error",
-          messages: ["You have no access to this content or action"]
+          status: 'error',
+          messages: ['You have no access to this content or action']
         }
       end
+    end
+
+    def single_track_playlist_response(track_playlist)
+      has_voted_up = current_user.voted_up_on? track_playlist
+      has_voted_down = current_user.voted_down_on? track_playlist
+
+      track_playlist
+        .attributes
+        .except('added_by_id', 'cached_votes_total', 'cached_votes_up', 'cached_votes_down', 'cached_votes_score')
+        .merge({
+                 status: 'success',
+                 added_by: {
+                   id: track_playlist.added_by_id,
+                   username: track_playlist.added_by_username
+                 },
+                 score: track_playlist.cached_votes_score,
+                 has_voted_up: has_voted_up,
+                 has_voted_down: has_voted_down
+               })
+    end
+
+    def readable_validation_errors(resource)
+      {
+        status: 'error',
+        messages: resource.errors.map { |k, v| "#{k.capitalize} #{v}" }
+      }
     end
   end
 end
