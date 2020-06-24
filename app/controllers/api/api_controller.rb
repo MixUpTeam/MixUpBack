0f9465dcf69ef5e26c986065ec9dd818a52449bd
Julie K <juliette.nada@gmail.com>
2020-06-24 16:04:18 +0200

    #46 function get current user (#47)
    * [add] user auth filter to all controller actions
    
    * [change] reponse method from application controller to api controller
    
    * [change] all front-sent user params to current user in playlists_controller and track_playlists_controller
    
    * [SKIP TRAVIS]

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,7 +1,7 @@
 module Api
   class ApiController < ApplicationController
+    before_action :authenticate_user!
     rescue_from ActionController::ParameterMissing, with: :missing_params_error_response
-    # TODO: (note for the front)need to catch ActionDispatch::Http::Parameters::ParseError in axios
 
     def missing_params_error_response(exception)
       render json: {
@@ -9,6 +9,43 @@ module Api
         messages: [
           exception.message.capitalize
         ]
+      }
+    end
+
+    def authenticate_user!
+      if current_user
+        @user = current_user
+      else
+        render json: {
+          status: 'error',
+          messages: ['You have no access to this content or action']
+        }
+      end
+    end
+
+    def single_track_playlist_response(track_playlist)
+      has_voted_up = current_user.voted_up_on? track_playlist
+      has_voted_down = current_user.voted_down_on? track_playlist
+
+      track_playlist
+        .attributes
+        .except('added_by_id', 'cached_votes_total', 'cached_votes_up', 'cached_votes_down', 'cached_votes_score')
+        .merge({
+                 status: 'success',
+                 added_by: {
+                   id: track_playlist.added_by_id,
+                   username: track_playlist.added_by_username
+                 },
+                 score: track_playlist.cached_votes_score,
+                 has_voted_up: has_voted_up,
+                 has_voted_down: has_voted_down
+               })
+    end
+
+    def readable_validation_errors(resource)
+      {
+        status: 'error',
+        messages: resource.errors.map { |k, v| "#{k.capitalize} #{v}" }
       }
     end
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,4 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
-
   respond_to :json
-
-  def single_track_playlist_response(track_playlist)
-    has_voted_up = User.first.voted_up_on? track_playlist # TODO: To be modified with current_user
-    has_voted_down = User.first.voted_down_on? track_playlist # TODO: To be modified with current_user
-
-    track_playlist
-      .attributes
-      .except('added_by_id', 'cached_votes_total', 'cached_votes_up', 'cached_votes_down', 'cached_votes_score')
-      .merge({
-               status: 'success',
-               added_by: {
-                 id: track_playlist.added_by_id,
-                 username: track_playlist.added_by_username
-               },
-               score: track_playlist.cached_votes_score,
-               has_voted_up: has_voted_up,
-               has_voted_down: has_voted_down
-             })
-  end
-
-  def readable_validation_errors(resource)
-    {
-      status: 'error',
-      messages: resource.errors.map { |k, v| "#{k.capitalize} #{v}" }
-    }
-  end
 end
